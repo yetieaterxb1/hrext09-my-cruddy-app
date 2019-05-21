133568a36bd0428648b82083e596638965f9c9da
Grant Steinke <GrantSteinke@grants-mbp-9.lan>
2019-05-21 17:22:06 -0600

    finished main functions

--- app/app.js
+++ app/app.js
@@ -8,48 +8,6 @@
 - [ ] UI/UX considerations (how are we going to use this)
 
 */
-
-
-
-//updateBeer
-//getBeer
-
-//newBeer function
-var Beer = function(name, brewery, rating){
-  this.db = localStorage.beers
-  this.beer = {
-    name: name,
-    brewery: brewery,
-    rating: rating 
-  }
-  this.save = function(){
-    this.db.append(this.beer)
-  }
-  this.delete = function(){
-    for(let i in this.db){
-      if(this.db[i].beer.name == this.beer.name){
-        this.db[i] = null
-      }
-    }
-  }
-}
-//renderBeerList
-var renderBeerList = function(brewery){
-  localStorage.beers.forEach(function(beer){
-    if (beer.beer.brewery === brewery){
-      return beer;
-    }
-  })
-}
-//deleteBeer
-var deleteBeer = function(beer){
-  localStorage.beers.forEach(function(beer){
-    if (beer.beer.name === beer){
-      beer.delete;
-    }
-  })
-}
-
 //localStorage interaction function
 //get item
 var getItem = function(key) {
@@ -82,31 +40,112 @@ var keyExists = function(key) {
 }
 
 
+
+//updateBeer
+//getBeer
+
+//newBeer function
+// var Beer = function(name, brewery, rating){
+//   this.db = localStorage.beers
+//   this.beer = {
+//     name: name,
+//     brewery: brewery,
+//     rating: rating 
+//   }
+//   this.save = function(){
+//     this.db.append(this.beer)
+//   }
+//   this.delete = function(){
+//     for(let i in this.db){
+//       if(this.db[i].beer.name == this.beer.name){
+//         this.db[i] = null
+//       }
+//     }
+//   }
+// }
+
+//renderBeerList
+var renderBeerList = function(brewery){
+  localStorage.beers.forEach(function(beer){
+    if (beer.beer.brewery === brewery){
+      return beer;
+    }
+  })
+}
+
+var getDataBase = function(dbkey){
+  return JSON.parse(localStorage.getItem(dbkey))
+}
+console.log(getDataBase('beers'));
+
+var updateDatabase = function(dbkey, db){ 
+  localStorage.setItem(dbkey,JSON.stringify(db))
+}
+updateDatabase('beers', [{name: 'coors', brewery: 'Coors', rating: 4}])
+console.log(getDataBase('beers'));
+
+//deleteBeer
+var deleteBeer = function(name){
+  var y = getDataBase('beers')
+  var filtered = y.filter(function(el) { return el.name != name; }); 
+  updateDatabase('beers', filtered)      
+}
+var getBeer = function(name){
+  var y = getDataBase(name)
+  for (let i in y){
+    if(y[i].name === name){
+      return y[i];
+    }
+  }
+}
+var addBeer = function(name,brewery,rating){
+  var y = getBeer(name)
+  var x = {name: name, brewery: brewery, rating: rating};
+  // if (x.name !== y.name && x.brewery !== y.brewery){
+    var beers = getDataBase('beers')
+    beers.push(x);
+    updateDatabase('beers', beers)
+  //}
+}
+
+var updateBeer = function(name,brewery,rating){
+  deleteBeer(name)
+  var x = {name: name, brewery: brewery, rating: rating};
+  var beers = getDataBase('beers')
+  beers.push(x);
+  updateDatabase('beers', beers)
+}
+
+var filterBrewery = function(brewery){
+  var y = getDataBase('beers')
+  var filtered = y.filter(function(el) { return el.brewery = brewery; }); 
+  return filtered 
+}
+var sorter = function(array){
+  array.sort(function(a, b){return b.rating-a.rating})
+  return array; 
+}
+var filterBeers = function(name){
+  var y = getDataBase('beers')
+  var filtered = y.filter(function(el) { return el.rating = rating; }); 
+  return filtered 
+}
+updateBeer('coors', 'Coors', 3)
+console.log(getDataBase('beers'));
+addBeer('MilkStout', 'LeftHand', 4)
+console.log(getDataBase('beers'));
+addBeer('MilkStout', 'LeftHand', 3)
+addBeer('MilkStout', 'LeftHand', 2)
+addBeer('MilkStout', 'LeftHand', 1)
+addBeer('MilkStout', 'LeftHand', 2)
+console.log(sorter(getDataBase('beers')))
+
+
+
+
 ///////////////////////////////////////////
 //event handlers for the buttons and ... possibly the inputboxes
   //preventdefault on button clicks
 $(document).ready(function() {
-  $('#createButton').click(function(event) {
-    event.preventDefault();
-
-    var currentKey = $("#keyInput").val();
-    var currentValue = $("#valueInput").val();
-    if (keyExists(currentKey)) {
-      //current key exists, do something error-handle-y
-    } else {
-      createItem(currentKey, currentValue);
-    }
-  });
-
-  $('#updateButton').click(function(event) {
-    event.preventDefault();
-
-    var currentKey = $("#keyInput").val();
-    var currentValue = $("#valueInput").val();
-    if (keyExists(currentKey)) {
-      updateItem(currentKey, currentValue);
-    } else {
-      //current key doesnt exist, do stuff
-    }
-  });
+  
 });
